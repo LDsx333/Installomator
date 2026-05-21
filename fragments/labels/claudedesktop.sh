f73bad6b61feb68d1b720870901df94dce797212
LDsx333 <81577045+LDsx333@users.noreply.github.com>
2026-05-21 17:01:51 +0200

    Delete claudedesktop.sh

--- fragments/labels/claudedesktop.sh
+++
@@ -1,8 +0,0 @@
-claudedesktop)
-    name="Claude"
-    appName="Claude.app"
-    type="zip"
-    downloadURL=$(getJSONValue "$(curl -fs https://downloads.claude.ai/releases/darwin/universal/RELEASES.json)" ".releases[0].updateTo.url")
-    appNewVersion=$(getJSONValue "$(curl -fsL https://downloads.claude.ai/releases/darwin/universal/RELEASES.json)" "releases[0].updateTo.version")
-    expectedTeamID="Q6L2SF6YDW"
-    ;;
